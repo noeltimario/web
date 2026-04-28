81e332e876485f74a6f00772cd62c9ba1595bb3f
Noel Timario <120074816+noeltimario@users.noreply.github.com>
2026-04-28 15:55:04 +0800

    Update database connection pool configuration
    Added configuration options for connection pool settings.

--- UC-SmartHelp-hosting-master/UC-SmartHelp-main/server.js
+++ UC-SmartHelp-hosting-master/UC-SmartHelp-main/server.js
@@ -22,11 +22,16 @@ app.use(cors({
 const db = mysql.createPool({
   uri: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
+    // Kani nga part importante para sa Aiven
+    rejectUnauthorized: false,
   },
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  maxIdle: 10, 
+  idleTimeout: 60000,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
 
 // Test Database Connection
